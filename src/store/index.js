8cc03d9b7d9d7537832e62e01842f5b5c65dfaec
Diego Cândido <diegocndd4@gmail.com>
2021-11-23 13:39:01 -0300

    Persistência dos dados no navegador

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,20 @@
 import app from './ducks';
 import {createStore, combineReducers} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'voter',
+    storage,
+};
 
 const reducers = combineReducers({
     app,
 });
 
-export const Store = createStore(reducers);
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export {store, persistor};
