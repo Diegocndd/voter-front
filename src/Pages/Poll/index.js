7779ab2b2e6fa2421c32021783f7cb9fd70f83b0
Diego Cândido <diegocndd4@gmail.com>
2021-12-05 00:10:04 -0300

    Criada a tela de detalhes da votação

--- src/Pages/Poll/index.js
+++ src/Pages/Poll/index.js
@@ -4,6 +4,7 @@ import getPoll from '../../services/public/getPoll';
 import getAlternatives from '../../services/public/getAlternatives';
 import FingerprintJS from '@fingerprintjs/fingerprintjs'
 import './styles.css';
+import NotFoundImage from '../../assets/img/not-found-poll.png';
 import visiterAlreadyVote from '../../services/public/visiterAlreadyVote';
 import votePoll from '../../services/public/votePoll';
 import getUser from '../../services/public/getUser';
@@ -19,6 +20,7 @@ function Poll() {
   const [pollCreator, setPollCreator] = useState('');
   const [alternatives, setAlternatives] = useState([]);
   const [chartAlternatives, setChartAlternatives] = useState([]);
+  const [errorNotPollFound, setErrorNotPollFound] = useState(true);
   const [visitorId, setVisitorId] = useState([]);
   const [userVoted, setUserVoted] = useState(false);
   const [chosenAlternativeId, setChosenAlternativeId] = useState(null);
@@ -27,18 +29,20 @@ function Poll() {
   useEffect(() => {
     let idPoll = queryParams.get('id');
     setIdPoll(idPoll);
-    (async () => {
-      const fp = await fpPromise;
-      const result = await fp.get();
-      setVisitorId(result.visitorId);
-
-      visiterAlreadyVote({idPoll, idVisitor: result.visitorId})
-        .then(res => setUserVoted(res.data === "True"))
-        .catch(err => setUserVoted(false));
-    })();
 
     getPoll(idPoll)
       .then(res => {
+        (async () => {
+          const fp = await fpPromise;
+          const result = await fp.get();
+          setVisitorId(result.visitorId);
+    
+          visiterAlreadyVote({idPoll, idVisitor: result.visitorId})
+            .then(res => setUserVoted(res.data === "True"))
+            .catch(err => setUserVoted(false));
+        })();
+
+        setErrorNotPollFound(false);
         setPollData(res.data);
         getAlternatives(idPoll)
           .then(result => {
@@ -48,7 +52,8 @@ function Poll() {
                 .then(resultUser => setPollCreator(resultUser.data))
             }
           })
-      });
+      })
+      .catch(err => setErrorNotPollFound(true));
   }, []);
 
   useEffect(() => {
@@ -81,32 +86,42 @@ function Poll() {
     <div>
       <Header />
       <div id="container-public-poll">
-        <div id="container-header-poll">
-          <p id="title-poll-vote">{pollData.title}</p>
-          <p id="username-author-poll">por {pollCreator}</p>
-        </div>
-        {userVoted ? (
-          <div id="container-alert-visitor">
-            <p id="alert-visitor-voted">Resultado Parcial da Votação</p>
-            <ChatPoll alternatives={chartAlternatives} />
-            <p id="alert-visitor-voted">Você já votou nessa enquete. Não é possível votar novamente!</p>
-          </div>
+        {!errorNotPollFound ? (
+          <div id="container-public-poll2">
+            <div id="container-header-poll">
+              <p id="title-poll-vote">{pollData.title}</p>
+              <p id="username-author-poll">por {pollCreator}</p>
+            </div>
+            {userVoted ? (
+              <div id="container-alert-visitor">
+                <p id="alert-visitor-voted">Resultado Parcial da Votação</p>
+                <ChatPoll alternatives={chartAlternatives} />
+                <p id="alert-visitor-voted">Você já votou nessa enquete. Não é possível votar novamente!</p>
+              </div>
+            ) : (
+              <>
+                <div style={{marginBottom: 70, marginTop: 70}}>
+                  {alternatives.map(alternative => {
+                    return (
+                      <div className="alternative-container" onChange={chosenAlternative.bind(this)}>
+                        <p>{alternative.description}</p>
+                        <input type="radio" id="public" name="alternative" value={alternative.id_alternative} />
+                      </div>
+                    );
+                  })}
+                </div>
+                <div id="button-vote-poll" onClick={() => voteAlternative()}>
+                  <p>Votar</p>
+                </div>
+              </>
+            )}
+          </div>  
         ) : (
-          <>
-            <div style={{marginBottom: 70, marginTop: 70}}>
-              {alternatives.map(alternative => {
-                return (
-                  <div className="alternative-container" onChange={chosenAlternative.bind(this)}>
-                    <p>{alternative.description}</p>
-                    <input type="radio" id="public" name="alternative" value={alternative.id_alternative} />
-                  </div>
-                );
-              })}
-            </div>
-            <div id="button-vote-poll" onClick={() => voteAlternative()}>
-              <p>Votar</p>
-            </div>
-          </>
+          <div style={{marginTop: -40}}>
+            <p id="text-not-found-poll">Não encontramos a votação!</p>
+            <p id="subtext-not-found-poll">Verifique se o endereço está correto.</p>
+            <img src={NotFoundImage} id="not-found-image-poll"></img>
+          </div>
         )}
       </div>
     </div>
--- src/Pages/PollDetail/index.js
+++ src/Pages/PollDetail/index.js
@@ -5,12 +5,32 @@ import QRCode from 'react-qr-code';
 import { useEffect, useState } from 'react';
 import getPoll from '../../services/public/getPoll';
 import getAlternatives from '../../services/public/getAlternatives';
+import ChartPoll from '../../Components/ChartPoll';
+import deletePoll from '../../services/deletePoll';
+import { useNavigate } from 'react-router';
 
 function PollDetail() {
+  const navigation = useNavigate();
   const [pollData, setPollData] = useState({});
   const [alternatives, setAlternatives] = useState([]);
+  const [chartAlternatives, setChartAlternatives] = useState(null);
+  const [localTime, setLocalTime] = useState(null);
+  const [limitDate, setLimitDate] = useState(null);
+  const [isClosedDate, setIsClosedDate] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
   const queryParams = new URLSearchParams(window.location.search);
   const url = `http://localhost:3000/poll?id=${queryParams.get('id')}`;
+
+  const formatAlternatives = (alternativesOpts) => {
+    let alternativesFormat = [];
+
+    alternativesOpts.map(alt => {
+      const {description, qty_votes} = alt;
+      alternativesFormat.push([description, qty_votes]);
+    })
+
+    setChartAlternatives(alternativesFormat);
+  };
 
   useEffect(() => {
     let idPoll = queryParams.get('id');
@@ -22,28 +42,109 @@ function PollDetail() {
           .then(result => {
             if (result.status === 200) {
               setAlternatives(result.data);
+              formatAlternatives(result.data);
             }
           })
-      });
+      })
+      .catch(err => navigation('/dashboard'));
   }, []);
+
+  const ISOStringToNormalDate = (oldDate) => {
+    return oldDate.split('T')[0] + ' ' + oldDate.split('T')[1].split('.')[0];
+  }
+
+  useEffect(() => {
+    const limitDatePoll = pollData.limit_date;
+    if (limitDatePoll) {
+      const tzoffset = (new Date()).getTimezoneOffset() * 60000;
+      const localISODate = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+      const actualDate = ISOStringToNormalDate(localISODate);
+      const limitDate = ISOStringToNormalDate(limitDatePoll);
+  
+      setLocalTime(localISODate);
+      setLimitDate(limitDate);
+    
+      const date1Updated = new Date(actualDate.replace(/-/g,'/'));  
+      const date2Updated = new Date(limitDate.replace(/-/g,'/'));
+  
+      setIsClosedDate(date1Updated > date2Updated)
+    }
+  }, [pollData]);
+
+  const formatDate = (date) => {
+    // yyyy-mm-dd hh:mm:ss to dd/mm/yyyy hh:mm
+    let newDate = date;
+
+    if (date) {
+      const year = date.split(' ')[0].split('-')[0];
+      const month = date.split(' ')[0].split('-')[1];
+      const day = date.split(' ')[0].split('-')[2];
+      const hour = date.split(' ')[1].split(':')[0];
+      const minute = date.split(' ')[1].split(':')[1];
+      newDate = day + '/' + month + '/' + year + ' ' + hour + ':' + minute;
+    }
+
+    return newDate;
+  }
+
+  const confirmDeletePoll = () => {
+    deletePoll(queryParams.get('id'))
+      .then(res => {
+        if (res.status === 200) {
+          navigation('/dashboard');
+        }
+      });
+  }
 
   return (
     <div>
       <Header />
+      {openModal ? (
+        <div className='modal-delete-poll'>
+              <div style={{marginBottom: 20}}>
+                <p id="title-modal-delete-poll">Tem certeza que deseja deletar a votação?</p>
+                <p>Essa ação é irreversível! Você não terá mais acesso aos dados dessa votação.</p>
+              </div>
+              <div id="footer-buttons-container">
+                <div id="cancel-button-modal" onClick={() => setOpenModal(false)}>Cancelar</div>
+                <div id="delete-button-modal" onClick={() => confirmDeletePoll()}>Deletar</div>
+              </div>
+        </div>
+      ) : null}
       <div id="content-poll-detail">
         <p id="title-poll-detail">{pollData.title}</p>
-        <div id="qr-code-poll">
-          <QRCode value={url} size={200} />
-        </div>
-        <div id="url-poll-container" onClick={()=> window.open(url, "_blank")}>
-          <p id="url-poll-text">{url}</p>
-        </div>
         <div>
           {pollData.qty_votes === 0 ? (
             <p className="votes-poll-text">Sua votação ainda não possui nenhum voto!</p>
           ) : (
             <p className="votes-poll-text">Sua votação já possui <b>{pollData.qty_votes}</b> {pollData.qty_votes > 1 ? 'votos' : 'voto'} </p>
           )}
+        </div>
+
+        <div id="datelimit-poll">
+          {isClosedDate ? (
+            <p id="datelimit-poll-text" style={{color: '#F01E1E'}}><b>Votação encerrada!</b></p>
+          ) : (
+            <p id="datelimit-poll-text">A votação encerrá dia <b>{formatDate(limitDate)}</b></p>
+          )}
+        </div>
+
+        <div id="qr-code-poll">
+          <QRCode value={url} size={200} />
+        </div>
+
+        <div id="url-poll-container" onClick={()=> window.open(url, "_blank")}>
+          <p id="url-poll-text">{url}</p>
+        </div>
+
+        <div>
+          {chartAlternatives ? (
+            <ChartPoll alternatives={chartAlternatives} />
+          ) : null}
+        </div>
+
+        <div id="delete-poll-container" onClick={() => setOpenModal(true)}>
+          <p>EXCLUIR VOTAÇÃO</p>
         </div>
       </div>
     </div>
