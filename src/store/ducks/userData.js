7c869c6f244eca599de2b0c8669dc7e256fe7e4a
Diego Cândido <diegocndd4@gmail.com>
2023-10-21 09:34:48 -0300

    updates

--- src/store/ducks/userData.js
+++ src/store/ducks/userData.js
@@ -1,35 +1,36 @@
-
 export const Types = {
-  SET_DATA: 'SET_DATA',
-  CLEAR_DATA: 'CLEAR_DATA',
+  SET_DATA: "SET_DATA",
+  CLEAR_DATA: "CLEAR_DATA",
 };
-    
+
 const initialState = {
   data: {},
 };
-  
+
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case Types.SET_DATA:
-      return {data: action.payload.data};
+      return { data: action.payload.data };
     case Types.CLEAR_DATA:
-      return {data: {}};
+      return { data: {} };
+    case Types.SET_DATA:
+      return { data: {} };
     default:
       return state;
   }
 }
-    
+
 export function setUserData(data) {
   return {
     type: Types.SET_DATA,
     payload: {
-      data
+      data,
     },
-  }
+  };
 }
 
 export function clearUserData() {
   return {
     type: Types.CLEAR_DATA,
-  }
+  };
 }
